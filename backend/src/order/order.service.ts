eb641f0f69bc3596950a3c738c0a2a959bb7375f
AnastasiaS99 <kuznetzova.a2013@yandex.ru>
2026-06-14 15:17:07 +0300

    Исправление тестов3

--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -2,7 +2,9 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateOrderDto, OrderResponseItemDto } from './dto/order.dto';
 import { ApiResponseDto } from '../films/dto/films.dto';
 import { FilmsRepository } from '../repository/films.repository';
+import * as crypto from 'crypto';
 
+(global as any).crypto = crypto;
 
 @Injectable()
 export class OrderService {
